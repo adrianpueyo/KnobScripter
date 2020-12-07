bf9349639ffb407e314a1fa77a8a76ea0f459a9e
Adrian Pueyo <pueyovfx@gmail.com>
2020-12-07 09:32:20 +0000

    Update knob_scripter.py

--- KnobScripter/knob_scripter.py
+++ KnobScripter/knob_scripter.py
@@ -39,7 +39,7 @@ try:
     if nuke.NUKE_VERSION_MAJOR < 11:
         from PySide import QtCore, QtGui, QtGui as QtWidgets
         from PySide.QtCore import Qt
-    else:2
+    else:
         from PySide2 import QtWidgets, QtGui, QtCore
         from PySide2.QtCore import Qt
 except ImportError:
